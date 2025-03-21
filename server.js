72471362c8ce6a44a3425e9fbfcc3fa55e899da3
Benv1984 <47241499+Benv1984@users.noreply.github.com>
2025-03-21 16:41:36 +0200

    Force update all files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import userRoutes from './routes/user.routes.js';
 import postRoutes from './routes/post.routes.js';
 import notificationRoutes from './routes/notification.routes.js';
 
-import connectMongoDB from './db/connectMongoDb.js';
+//import connectMongoDB from './db/connectMongoDb.js';
 
 dotenv.config();
 
