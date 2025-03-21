14a58ae61c3beb0169509deccad3aa8e3a4d5e2f
Benv1984 <47241499+Benv1984@users.noreply.github.com>
2025-03-21 15:18:43 +0200

    just restarted

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from "cloudinary";
+import cors from 'cors'
+
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import postRoutes from './routes/post.routes.js';
+import notificationRoutes from './routes/notification.routes.js';
+
+import connectMongoDB from './db/connectMongoDb.js';
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "10mb" })); // Adjust as needed
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+})
